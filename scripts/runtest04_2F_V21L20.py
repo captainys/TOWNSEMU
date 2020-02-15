cf1aa0f939a114f1afdb4d16d80e4b8d74dd4407
CaptainYS <PEB01130@nifty.com>
2020-02-14 22:44:40 -0500

    Working on fopen, fgets, and fclose.  fgets falls into an infinite recursion.

---
+++ scripts/runtest04_2F_V21L20.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+import shutil
+
+import build
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+BUILDDIR=os.path.join(THISDIR,"..","build")
+SRCDIR=os.path.join(THISDIR,"..","src")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_2F")
+DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
+
+
+
+def Run():
+	os.chdir(BUILDDIR)
+	subprocess.Popen([
+		"./main_cui/main_cui.exe",
+		ROMDIR,
+		"-FD0",
+		os.path.join(DISKDIR,"testset02.bin"),
+		"-SYM",
+		"../symtables/TESTSET02.txt",
+	]).wait()
+
+
+
+if __name__=="__main__":
+	build.Run()
+	Run()
