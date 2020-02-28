b0464709ce646ed185f2ae50f145964e783f39db
CaptainYS <PEB01130@nifty.com>
2020-02-28 14:38:38 -0500

    Adding CDROM unit tests

---
+++ scripts/cdtest_2F_V2.1L20.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import shutil
+
+import build
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+BUILDDIR=os.path.join(THISDIR,"..","build")
+SRCDIR=os.path.join(THISDIR,"..","src")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_2F")
+DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
+
+
+
+def Run():
+	os.chdir(BUILDDIR)
+	subprocess.Popen([
+		"./main_cui/main_cui.exe",
+		ROMDIR,
+		"-FD0",
+		os.path.join(DISKDIR,"testcdV2.1L20.bin"),
+		"-CD",
+		os.path.join(THISDIR,"..","testdata","ISOIMG","TESTDISC.ISO"),
+		"-SYM",
+		"../symtables/CDTEST_2F_V2.1L20.txt",
+		"-DEBUG",
+		# "-PAUSE",
+	]).wait()
+
+
+
+if __name__=="__main__":
+	build.Run()
+	Run()
