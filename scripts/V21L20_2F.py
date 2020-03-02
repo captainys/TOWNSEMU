419f55d1eb61bf7af10eb4b48ab34397b3041e7d
CaptainYS <PEB01130@nifty.com>
2020-03-02 08:52:33 -0500

    Changed SYM file names.

--- scripts/V21L20_2F.py
+++ scripts/V21L20_2F.py
@@ -4,11 +4,13 @@ import shutil
 
 import build
 
+TOWNSTYPE="2F"
+
 THISFILE=os.path.realpath(__file__)
 THISDIR=os.path.dirname(THISFILE)
 BUILDDIR=os.path.join(THISDIR,"..","build")
 SRCDIR=os.path.join(THISDIR,"..","src")
-ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_2F")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_"+TOWNSTYPE)
 DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
 
 
@@ -23,7 +25,7 @@ def Run():
 		"-CD",
 		os.path.join(THISDIR,"..","testdata","ISOIMG","TESTDISC.ISO"),
 		"-SYM",
-		"../symtables/V2.1L30.txt",
+		"../symtables/V2.1L20_"+TOWNSTYPE+".txt",
 		"-DEBUG",
 		"-PAUSE",
 	]).wait()
--- scripts/V21L30_2F.py
+++ scripts/V21L30_2F.py
@@ -4,11 +4,13 @@ import shutil
 
 import build
 
+TOWNSTYPE="2F"
+
 THISFILE=os.path.realpath(__file__)
 THISDIR=os.path.dirname(THISFILE)
 BUILDDIR=os.path.join(THISDIR,"..","build")
 SRCDIR=os.path.join(THISDIR,"..","src")
-ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_2F")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_"+TOWNSTYPE)
 DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
 
 
@@ -21,7 +23,7 @@ def Run():
 		"-FD0",
 		os.path.join(DISKDIR,"V2.1L30.bin"),
 		"-SYM",
-		"../symtables/V2.1L30.txt",
+		"../symtables/V2.1L30"+TOWNSTYPE+".txt",
 		"-DEBUG",
 		"-PAUSE",
 	]).wait()
