97c051310ad9c320ee60eb4e6fd87aebc531f0c5
CaptainYS <PEB01130@nifty.com>
2020-03-06 00:23:33 -0500

    Added Towns OS V1.1 CD Boot scripts.

---
+++ scripts/V11L30CDBOOT.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import shutil
+
+import build
+
+TOWNSTYPE="MX"
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+BUILDDIR=os.path.join(THISDIR,"..","build")
+SRCDIR=os.path.join(THISDIR,"..","src")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_"+TOWNSTYPE)
+DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
+
+
+
+def Run():
+	os.chdir(BUILDDIR)
+	subprocess.Popen([
+		"./main_cui/main_cui.exe",
+		ROMDIR,
+		"-FD0",
+		os.path.join(DISKDIR,"V2.1L20.bin"),
+		"-CD",
+		"D:/ISOImageSubset/TOSV1.1L30.iso",
+		"-SYM",
+		"../symtables/V1.1L30CDBOOT_"+TOWNSTYPE+".txt",
+		"-DEBUG",
+		"-PAUSE",
+	]).wait()
+
+
+
+if __name__=="__main__":
+	build.Run()
+	Run()
