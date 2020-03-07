936b27776fff947f4013ac453de812c48150a712
CaptainYS <PEB01130@nifty.com>
2020-03-07 17:59:38 -0500

    Changed the interpretation of LOAD bits of YM2612 register 27H.  I interpret it as: the timer counter is reloaded on positive edge of the LOAD bit.  Therefore, writing LOAD=1 when LOAD is already 1 does nothing.  When the timer is up, LOAD is set back to 0.  If I interpret LOAD bits as described in FM Towns Technical Guidebook 3rd Edition pp.201, Towns OS reloads counter of B when reloading counter of A (or vise versa).  As a result, timer A or B whichever have longer cycle never becomes up.  With the new interpretation, mouse cursor works when both timer A and timer B are running.

--- src/ym2612/ym2612.cpp
+++ src/ym2612/ym2612.cpp
@@ -43,17 +43,46 @@ void YM2612::Reset(void)
 }
 void YM2612::WriteRegister(unsigned int reg,unsigned int value)
 {
-	state.reg[reg&255]=value;
+	reg&=255;
+	auto prev=state.reg[reg];
+	state.reg[reg]=value;
 	if(REG_TIMER_CONTROL==reg)
 	{
-		if(value&1) // Load Timer A
+		// [2] pp. 202 RESET bits will be cleared immediately after set.
+		// .... I interpret it as RESET bits are always read to zero.
+		state.reg[REG_TIMER_CONTROL]&=0xCF;
+
+		// LOAD bits are mysterious.
+		// [2] pp.201 tells that writing 1 to LOAD bit resets the counter and start counting.
+		// Towns OS's behavior does not seem to be agree with it.
+		// Timer A interrupt handler writes 0x3F to register 0x27.  If I implement as [2] pp.201,
+		// Timer B counter is reset when Timer A is up, or vise versa.
+		// Slower one of Timer A or B will never be up.
+		//
+		// There are some possibilities:
+		// (1) The timer counter resets on the rising edge of LOAD.  When timer is up, LOAD will be cleared.
+		//     Therefore, writing LOAD=1 when LOAD is already 1 does nothing.
+		// (2) Towns OS writes (0118:[0727H])|0x15 for Timer A or (0118:[0727H])|0x2A for Timer B for resetting the counter.
+		//     0118:[0727H] is a cached value (or read back) from YM2612 register 27H.  If LOAD bits are always zero when
+		//     read, it won't reset the timer when resetting the other timer.
+		// (3) The timer counter is reloaded on LOAD=1 only if the timer is up.
+        //
+		// There are some web sites such as:
+		//   https://plutiedev.com/ym2612-registers#reg-27
+		//   https://www.smspower.org/maxim/Documents/YM2612
+		//   https://wiki.megadrive.org/index.php?title=YM2612_Registers
+		// suggesting that LOAD bit means the timer is running.  If so, I guess (2) is unlikely.
+		// With my elementary knowledge in FPGA programming, (1) looks to be more straight-forward.
+		// Currently I go with (1).
+
+		if(0==(prev&1) && 0!=(value&1)) // Load Timer A
 		{
 			unsigned int countHigh=state.reg[REG_TIMER_A_COUNT_HIGH];
 			unsigned int countLow=state.reg[REG_TIMER_A_COUNT_LOW];
 			auto count=(countHigh<<2)|(countLow&3);
 			state.timerCounter[0]=count*TIMER_A_PER_TICK;
 		}
-		if(value&2) // Load Timer B
+		if(0==(prev&2) && 0!=(value&2)) // Load Timer B
 		{
 			state.timerCounter[1]=(unsigned int)(state.reg[REG_TIMER_B_COUNT])*TIMER_B_PER_TICK;
 		}
@@ -89,22 +118,29 @@ void YM2612::Run(unsigned long long int systemTimeInNS)
 	{
 		auto nTick=(systemTimeInNS-state.lastTickTimeInNS)/TICK_DURATION_IN_NS;
 		state.lastTickTimeInNS+=nTick*TICK_DURATION_IN_NS;
-		if(state.timerCounter[0]<NTICK_TIMER_A)
+		// See (1) in the above comment.
+		if(0!=(state.reg[REG_TIMER_CONTROL]&0x01))
 		{
 			state.timerCounter[0]+=nTick;
-			if(NTICK_TIMER_A<=state.timerCounter[0] &&
-			   0!=(state.reg[REG_TIMER_CONTROL]&0x04))
+			if(NTICK_TIMER_A<=state.timerCounter[0])
 			{
-				state.timerUp[0]=true;
+				state.reg[REG_TIMER_CONTROL]&=(~0x01);
+				if(0!=(state.reg[REG_TIMER_CONTROL]&0x04))
+				{
+					state.timerUp[0]=true;
+				}
 			}
 		}
-		if(state.timerCounter[1]<NTICK_TIMER_B)
+		if(0!=(state.reg[REG_TIMER_CONTROL]&0x02))
 		{
 			state.timerCounter[1]+=nTick;
-			if(NTICK_TIMER_B<=state.timerCounter[1] &&
-			   0!=(state.reg[REG_TIMER_CONTROL]&0x08))
+			if(NTICK_TIMER_B<=state.timerCounter[1])
 			{
-				state.timerUp[1]=true;
+				state.reg[REG_TIMER_CONTROL]&=(~0x02);
+				if(0!=(state.reg[REG_TIMER_CONTROL]&0x08))
+				{
+					state.timerUp[1]=true;
+				}
 			}
 		}
 	}
