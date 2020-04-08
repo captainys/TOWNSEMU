62019b01f2d1e8f0496b7011e65a4800cbb24955
CaptainYS <PEB01130@nifty.com>
2020-04-08 17:17:09 -0400

    Working on PCM

--- src/rf5c68/rf5c68.cpp
+++ src/rf5c68/rf5c68.cpp
@@ -4,5 +4,97 @@
 
 RF5C68::RF5C68()
 {
-	waveRAM.resize(WAVERAM_SIZE);
+	state.waveRAM.resize(WAVERAM_SIZE);
+	Clear();
+}
+
+void RF5C68::Clear(void)
+{
+	for(auto &b : state.waveRAM)
+	{
+		b=0;
+	}
+	for(auto &ch : state.ch)
+	{
+		ch.ENV=0;
+		ch.PAN=0;
+		ch.ST=0;
+		ch.FD=0;
+		ch.LS=0;
+	}
+	state.playing=false;
+	state.Bank=0;
+	state.CB=0;
+	state.chOnOff=0;
+}
+
+unsigned char RF5C68::WriteControl(unsigned char value)
+{
+	if(0x40&value)
+	{
+		state.CB=(value&7);
+	}
+	else
+	{
+		auto WB=(value&0x0f);
+		state.Bank=WB;
+		state.Bank<<=12;
+	}
+
+	unsigned char chStartPlay=0;
+	if(0x80&value)
+	{
+		if(true!=state.playing)
+		{
+			chStartPlay=state.chOnOff;
+		}
+		state.playing=true;
+	}
+	else
+	{
+		state.playing=false;
+	}
+	return chStartPlay;
+}
+unsigned char RF5C68::WriteChannelOnOff(unsigned char value)
+{
+	if(true==state.playing)
+	{
+		auto chStartPlay=((~state.chOnOff)&value);
+		state.chOnOff=value;
+		return chStartPlay;
+	}
+	else
+	{
+		state.chOnOff=value;
+		return 0;
+	}
+}
+void RF5C68::WriteENV(unsigned char value)
+{
+	state.ch[state.CB].ENV=value;
+}
+void RF5C68::WritePAN(unsigned char value)
+{
+	state.ch[state.CB].PAN=value;
+}
+void RF5C68::WriteFDL(unsigned char value)
+{
+	state.ch[state.CB].FD&=0xFF00;
+	state.ch[state.CB].FD|=value;
+}
+void RF5C68::WriteFDH(unsigned char value)
+{
+	state.ch[state.CB].FD&=0xFF;
+	state.ch[state.CB].FD|=(value<<8);
+}
+void RF5C68::WriteLSL(unsigned char value)
+{
+	state.ch[state.CB].LS&=0xFF00;
+	state.ch[state.CB].LS|=value;
+}
+void RF5C68::WriteLSH(unsigned char value)
+{
+	state.ch[state.CB].LS&=0xFF;
+	state.ch[state.CB].LS|=(value<<8);
 }
--- src/rf5c68/rf5c68.h
+++ src/rf5c68/rf5c68.h
@@ -10,12 +10,71 @@ class RF5C68
 public:
 	enum
 	{
-		WAVERAM_SIZE=65536
+		WAVERAM_SIZE=65536,
+		NUM_CHANNELS=8
 	};
 
-	std::vector <unsigned char> waveRAM;
+	class Channel
+	{
+	public:
+		unsigned char ENV,PAN,ST;
+		unsigned short FD,LS;
+	};
+	class State
+	{
+	public:
+		std::vector <unsigned char> waveRAM;
+		Channel ch[NUM_CHANNELS];
+
+		bool playing;          // Bit 7 of I/O 04F7H
+		unsigned char Bank;    // Bank x000H
+		unsigned char CB;      // Channel
+		unsigned char chOnOff; // I/O 04F8H
+	};
+	State state;
+
 
 	RF5C68();
+	void Clear(void);
+
+	/*! Writes to the control register.
+	    Returns channels that starts playing.
+	*/
+	unsigned char WriteControl(unsigned char value);
+
+	/*! Writes to the channel on/off register.
+	    Returns channels that starts playing. 
+	*/
+	unsigned char WriteChannelOnOff(unsigned char value);
+
+	/*! Writes to the ENV register. */
+	void WriteENV(unsigned char value);
+
+	/*! Writes to the ENV register. */
+	void WritePAN(unsigned char value);
+
+	/*! Writes to FDL register. */
+	void WriteFDL(unsigned char value);
+
+	/*! Writes to FDL register. */
+	void WriteFDH(unsigned char value);
+
+	/*! Writes to LSL register. */
+	void WriteLSL(unsigned char value);
+
+	/*! Writes to LSL register. */
+	void WriteLSH(unsigned char value);
+
+	/*! Writes to Wave RAM. */
+	inline void WriteWaveRAM(unsigned int offset,unsigned char value)
+	{
+		state.waveRAM[state.Bank+offset]=value;
+	}
+	/*! Read from Wave RAM. */
+	inline unsigned char ReadWaveRAM(unsigned int offset) const
+	{
+		return state.waveRAM[state.Bank+offset];
+	}
 };
 
 
