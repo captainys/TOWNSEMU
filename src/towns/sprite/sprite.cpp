31e6a94cdde8f38c5cf163fa483cb1f24f40bae8
CaptainYS <PEB01130@nifty.com>
2020-04-10 13:52:32 -0400

    Working on sprite

---
+++ src/towns/sprite/sprite.cpp
@@ -0,0 +1,161 @@
+/* LICENSE>>
+Copyright 2020 Soji Yamakawa (CaptainYS, http://www.ysflight.com)
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+<< LICENSE */
+#include "towns.h"
+#include "townsdef.h"
+#include "cpputil.h"
+#include "sprite.h"
+
+
+
+void TownsSprite::State::PowerOn(void)
+{
+	addressLatch=0;
+	for(auto &r : reg)
+	{
+		r=0;
+	}
+	spriteBusy=false;
+}
+void TownsSprite::State::Reset(void)
+{
+}
+
+TownsSprite::TownsSprite(class FMTowns *townsPtr) : Device(townsPtr)
+{
+	this->townsPtr=townsPtr;
+}
+
+/* virtual */ void TownsSprite::PowerOn(void)
+{
+	state.PowerOn();
+}
+/* virtual */ void TownsSprite::Reset(void)
+{
+	state.Reset();
+}
+/* virtual */ void TownsSprite::IOWriteByte(unsigned int ioport,unsigned int data)
+{
+	switch(ioport)
+	{
+	// case TOWNSIO_DPMD_SPRITEBUSY_SPRITEPAGE Not assigned to TownsSprite.
+
+	case TOWNSIO_SPRITE_ADDRESS://           0x450, // [2] pp.128
+		state.addressLatch=data&7;
+		break;
+	case TOWNSIO_SPRITE_DATA://              0x452, // [2] pp.128
+		state.reg[state.addressLatch]=data;
+		break;
+	}
+}
+/* virtual */ unsigned int TownsSprite::IOReadByte(unsigned int ioport)
+{
+	unsigned char data=0xff;
+	switch(ioport)
+	{
+	// case TOWNSIO_DPMD_SPRITEBUSY_SPRITEPAGE Not assigned to TownsSprite.
+
+	case TOWNSIO_SPRITE_ADDRESS://           0x450, // [2] pp.128
+		data=state.addressLatch;
+		break;
+	case TOWNSIO_SPRITE_DATA://              0x452, // [2] pp.128
+		if(state.addressLatch!=REG_DISPLAY_PAGE)
+		{
+			data=state.reg[state.addressLatch];
+		}
+		else
+		{
+			data=(state.reg[state.addressLatch]>>3); // [2] pp.130.  DP1 write -> Bit7, DP1 read -> Bit 4
+		}
+		break;
+	}
+	return data;
+}
+
+void TownsSprite::Render(unsigned char VRAMIn[],const unsigned char spriteRAM[]) const
+{
+	unsigned char *VRAMTop=VRAMIn+SPRITE_HALF_VRAM_SIZE*DisplayPage();
+	auto xOffset=HOffset(),yOffset=VOffset();
+	for(unsigned int spriteIndex=0; spriteIndex<NumSpritesToDraw(); ++spriteIndex)
+	{
+		auto indexPtr=spriteRAM+SPRITERAM_INDEX_OFFSET+(spriteIndex<<3);
+
+		const unsigned short indexInfo[4]=  // I hope the optimizer recognizes it.
+		{
+			(unsigned short)(indexPtr[0]|(indexPtr[1]<<8)),
+			(unsigned short)(indexPtr[2]|(indexPtr[3]<<8)),
+			(unsigned short)(indexPtr[4]|(indexPtr[5]<<8)),
+			(unsigned short)(indexPtr[6]|(indexPtr[7]<<8)),
+		};
+
+		unsigned int dstX=indexInfo[0];
+		unsigned int dstY=indexInfo[1];
+		const unsigned int attrib=indexInfo[2];
+		const unsigned int paletteInfo=indexInfo[3];
+
+		if(0!=(paletteInfo&PALETTE_DISP))  // Active LOW
+		{
+			continue;
+		}
+
+		if(0!=(attrib&ATTR_OFFS))
+		{
+			dstX+=xOffset;
+			dstY+=yOffset;
+		}
+		unsigned int patternIndex=attrib&ATTR_PAT_MASK;
+
+		const unsigned char ROT=((attrib&ATTR_ROT_MASK)>>ATTR_ROT_SHIFT);
+		const int xStep=(0!=(attrib&ATTR_SUX) ? 2 : 1);
+		const int yStep=(0!=(attrib&ATTR_SUY) ? 2 : 1);
+
+		if(0!=(paletteInfo&PALETTE_CTEN))
+		{
+			// 16-color paletted sprite
+			const unsigned int paletteIndex=paletteInfo&PALETTE_INDEX_MASK;
+			auto pattenPtr=spriteRAM+(patternIndex<<7);
+		}
+		else
+		{
+			// 32768-color sprite
+			patternIndex&=(~3);
+			auto srcPtr=spriteRAM+(patternIndex<<7);
+			if(dstX<256-SPRITE_DIMENSION && dstY<256-SPRITE_DIMENSION)
+			{
+				auto dstPtr=VRAMTop+SPRITE_VRAM_BYTES_PER_LINE*dstY;
+				for(unsigned int ptnY=0; ptnY<SPRITE_DIMENSION; ptnY+=yStep)
+				{
+					auto nextDstPtr=dstPtr+SPRITE_VRAM_BYTES_PER_LINE;
+					for(unsigned int ptnX=0; ptnX<SPRITE_DIMENSION; ptnX+=xStep)
+					{
+						unsigned int xTfm,yTfm;
+						Transform(xTfm,yTfm,ptnX,ptnY,ROT);
+
+						auto src=srcPtr+SPRITE_PTN32K_BYTES_PER_LINE*ptnY+(ptnX<<1);
+						if(0==(src[1]&0x80))
+						{
+							dstPtr[0]=src[0];
+							dstPtr[1]=src[1];
+						}
+						dstPtr+=2;
+					}
+					dstPtr=nextDstPtr;
+				}
+			}
+			else // Clipping not supported yet.
+			{
+			}
+		}
+	}
+}
---
+++ src/towns/sprite/sprite.h
@@ -0,0 +1,147 @@
+#ifndef SPRITE_IS_INCLUDED
+#define SPRITE_IS_INCLUDED
+/* { */
+
+#include "device.h"
+class TownsSprite : public Device
+{
+private:
+	class FMTowns *townsPtr;
+public:
+	virtual const char *DeviceName(void) const{return "SPRITE";}
+
+	enum
+	{
+		MAX_NUM_SPRITE_INDEX=1024,
+		MAX_NUM_SPRITE_PATTERN=896,
+		SPRITE_HALF_VRAM_SIZE=128*1024,
+		SPRITE_VRAM_BYTES_PER_LINE=512,
+		SPRITERAM_INDEX_OFFSET=0,
+		SPRITERAM_PALETTE_OFFSET=8192,
+		SPRITE_DIMENSION=16,
+		SPRITE_PTN32K_BYTES_PER_LINE=32,
+	};
+	enum
+	{
+		ATTR_OFFS=0x8000,
+		ATTR_ROT_MASK=0x7000,
+		ATTR_ROT_SHIFT=12,
+		ATTR_SUY=0x0800,
+		ATTR_SUX=0x0400,
+		ATTR_PAT_MASK=0x03FF,
+	};
+	enum
+	{
+		PALETTE_INDEX_MASK=0xFFF,
+		PALETTE_DISP=0x2000,
+		PALETTE_SPYS=0x4000,
+		PALETTE_CTEN=0x8000,
+	};
+
+	enum
+	{
+		REG_CONTROL0=0,
+		REG_CONTROL1=1,
+		REG_HORIZONTAL_OFFSET0=2,
+		REG_HORIZONTAL_OFFSET1=3,
+		REG_VERTICAL_OFFSET0=4,
+		REG_VERTICAL_OFFSET1=5,
+		REG_DISPLAY_PAGE=6,   // [2] pp.130  DP1 write -> Bit 7, DP1 read -> Bit 4.  state.reg[REG_DISPLAY_PAGE] retains a wirtten value.
+		REG_DUMMY=7,
+	NUM_REGS=8,
+	};
+
+	class State
+	{
+	public:
+		unsigned char addressLatch;
+		unsigned char reg[NUM_REGS];
+		bool spriteBusy;
+
+		void PowerOn(void);
+		void Reset(void);
+	};
+
+	State state;
+
+	TownsSprite(class FMTowns *townsPtr);
+
+	inline bool SpriteActive(void) const
+	{
+		return 0!=(state.reg[REG_CONTROL1]&0x80);
+	}
+	inline unsigned int NumSpritesToDraw(void) const
+	{
+		unsigned int n=((state.reg[REG_CONTROL1]<<8)|state.reg[REG_CONTROL0])&(MAX_NUM_SPRITE_INDEX-1);
+		if(0==n)
+		{
+			return MAX_NUM_SPRITE_INDEX;
+		}
+		return n;
+	}
+	inline unsigned int HOffset(void) const
+	{
+		return ((state.reg[REG_HORIZONTAL_OFFSET1]<<8)|state.reg[REG_HORIZONTAL_OFFSET0])&0x1FF;
+	}
+	inline unsigned int VOffset(void) const
+	{
+		return ((state.reg[REG_VERTICAL_OFFSET1]<<8)|state.reg[REG_VERTICAL_OFFSET0])&0x1FF;
+	}
+	inline unsigned int DisplayPage(void) const
+	{
+		return (state.reg[REG_DISPLAY_PAGE]>>7);
+	}
+
+
+	void Render(unsigned char VRAM[],const unsigned char spriteRAM[]) const;
+private:
+	inline static void Transform(unsigned int &X,unsigned int &Y,unsigned int x,unsigned int y,unsigned char ROT)
+	{
+		switch(ROT)
+		{
+		default:
+		case 0:
+			// No transformation
+			break;
+		case 1:
+			X=x;
+			Y=15-y;
+			break;
+		case 2:
+			X=15-x;
+			Y=y;
+			break;
+		case 3:
+			X=15-x;
+			Y=15-y;
+			break;
+		case 4:
+			X=y;
+			Y=x;
+			break;
+		case 5:
+			X=y;
+			Y=15-x;
+			break;
+		case 6:
+			X=15-y;
+			Y=x;
+			break;
+		case 7:
+			X=15-y;
+			Y=15-x;
+			break;
+		}
+	}
+
+public:
+	virtual void PowerOn(void);
+	virtual void Reset(void);
+
+	virtual void IOWriteByte(unsigned int ioport,unsigned int data);
+
+	virtual unsigned int IOReadByte(unsigned int ioport);
+};
+
+/* } */
+#endif
