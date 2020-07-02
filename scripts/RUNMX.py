460422a68a1434c433fd46669474799d8d3b2232
CaptainYS <PEB01130@nifty.com>
2020-07-02 14:00:30 -0400

    Added RUN2F and RUNMX scripts.

---
+++ scripts/RUNMX.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+import shutil
+import sys
+import sys
+
+import build
+
+TOWNSTYPE="MX"
+
+THISFILE=os.path.realpath(__file__)
+THISDIR=os.path.dirname(THISFILE)
+BUILDDIR=os.path.join(THISDIR,"..","build")
+SRCDIR=os.path.join(THISDIR,"..","src")
+ROMDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","ROM_"+TOWNSTYPE)
+DISKDIR=os.path.join(THISDIR,"..","..","TOWNSEMU_TEST","DISKIMG")
+
+
+
+def ExeExtension():
+	if sys.platform.startswith('win'):
+		return ".exe"
+	else:
+		return ""
+
+
+def Run(argv):
+	os.chdir(BUILDDIR)
+	subprocess.Popen([
+		"./main_cui/Tsugaru_CUI"+ExeExtension(),
+		ROMDIR,
+		"-SYM",
+		"../symtables/RUN"+TOWNSTYPE+".txt",
+		"-HD0",
+		os.path.join(DISKDIR,"hddimage.bin"),
+		"-CMOS",
+		"../testdata/CMOS.bin",
+		#"-DONTAUTOSAVECMOS",
+		#"-DEBUG",
+		#"-PAUSE",
+	]+argv).wait()
+
+
+
+if __name__=="__main__":
+	build.Run()
+	Run(sys.argv[1:])
