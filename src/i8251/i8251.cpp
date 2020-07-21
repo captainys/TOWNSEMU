cd39a44d01f3517b3bb0d655ff147812385e43c6
CaptainYS <PEB01130@nifty.com>
2020-07-20 21:17:19 -0400

    Working on RS232C

---
+++ src/i8251/i8251.cpp
@@ -0,0 +1,168 @@
+#include "i8251.h"
+
+
+
+i8251::i8251()
+{
+	Reset();
+}
+void i8251::Reset(void)
+{
+	state.immediatelyAfterReset=true;
+	state.stopBits=1;
+	state.preScale=64;
+
+	state.SCS=false;
+	state.ESD=false;
+	state.dataLength=8;
+	state.evenParity=true;
+	state.parityEnabled=false;
+
+	state.RxEN=false;
+	state.TxEN=false;
+	state.SYNDET=false;
+	state.FE=false;
+	state.OE=false;
+	state.PE=false;
+	state.TxEMPTY=false;
+	state.RTS=false;
+	state.DTR=false;
+
+	state.lastTxTime=0;
+	state.lastRxTime=0;
+}
+void i8251::VMWriteCommnand(unsigned char data)
+{
+	if(true==state.immediatelyAfterReset)
+	{
+		state.immediatelyAfterReset=false;
+		state.preScale=((data&MODESEL_B)>>MODESEL_B_SHIFT);    // B1, B2
+
+		state.parityEnabled=(0!=(data&MODESEL_PEN));   // PEN
+		state.evenParity=(0!=(data&MODESEL_EP));               // EP
+		state.dataLength=5+((data&MODESEL_L)>>MODESEL_L_SHIFT);// L1, L2
+
+		if(0==state.preScale) // Synchronous mode
+		{
+			state.SCS=(0!=(data&MODESEL_SYNC_SCS));
+			state.ESD=(0!=(data&MODESEL_SYNC_ESD));
+			if(nullptr!=clientPtr)
+			{
+				clientPtr->SetParity(state.parityEnabled,state.evenParity);
+				clientPtr->SetDataLength(state.dataLength);
+			}
+		}
+		else // Asynchronous mode
+		{
+			state.stopBits=((data&MODESEL_ASYNC_S)>>MODESEL_ASYNC_S_SHIFT);   // S1,S2
+			if(nullptr!=clientPtr)
+			{
+				clientPtr->SetStopBits(state.stopBits);
+				clientPtr->SetParity(state.parityEnabled,state.evenParity);
+				clientPtr->SetDataLength(state.dataLength);
+			}
+		}
+	}
+	else
+	{
+		if(0!=(data&CMD_WRITE_RESET))
+		{
+			Reset();
+		}
+		else
+		{
+			state.TxEN=(0!=(data&CMD_WRITE_TXEN));
+			state.DTR=(0!=(data&CMD_WRITE_DTR));
+			state.RxEN=(0!=(data&CMD_WRITE_RXEN));
+			state.BREAK=(0!=(data&CMD_WRITE_SBRK));
+			if(0!=(data&CMD_WRITE_CLRER))
+			{
+				state.FE=false;
+				state.OE=false;
+				state.PE=false;
+			}
+			state.RTS=(0!=(data&CMD_WRITE_RTS));
+
+			if(nullptr!=clientPtr)
+			{
+				clientPtr->Command(state.RTS,state.DTR,state.BREAK);
+			}
+
+			// Disregard EH.
+		}
+	}
+}
+unsigned char i8251::VMReadState(void) const
+{
+	unsigned char data=0;
+
+	data|=(TxRDY() ? STATUS_READ_TXRDY : 0);
+	data|=(RxRDY() ? STATUS_READ_RXRDY : 0);
+	data|=(state.TxEMPTY ? STATUS_READ_TXEMPTY : 0);
+	data|=(state.PE ? STATUS_READ_PE : 0);
+	data|=(state.OE ? STATUS_READ_OE : 0);
+	data|=(state.FE ? STATUS_READ_FE : 0);
+	data|=(state.SYNDET ? STATUS_READ_SYNDET : 0);
+	data|=(DSR() ? STATUS_READ_DSR : 0);
+
+	return data;
+}
+void i8251::VMWriteData(unsigned char data,long long int VMTime)
+{
+	state.lastTxTime=VMTime;
+	state.TxEMPTY=false;
+	state.TxRDY=false;
+	if(nullptr!=clientPtr)
+	{
+		clientPtr->Tx(data);
+	}
+}
+unsigned char i8251::VMReadData(long long int VMTime)
+{
+	state.lastRxTime=VMTime;
+	state.RxRDY=false;
+	if(nullptr!=clientPtr)
+	{
+		return clientPtr->Rx();
+	}
+	return 0xFF;
+}
+void i8251::Update(long long int newVMTime)
+{
+	if(true==state.TxEN && state.lastTxTime+state.nanoSecondsPerByte<newVMTime)
+	{
+		state.TxRDY=true;
+	}
+	if(true==state.RxEN && state.lastRxTime+state.nanoSecondsPerByte<newVMTime)
+	{
+		state.RxRDY=true;
+	}
+}
+bool i8251::RxRDY(void) const
+{
+	if(nullptr!=clientPtr)
+	{
+		return state.RxRDY && clientPtr->RxRDY();
+	}
+	return false;
+}
+bool i8251::TxRDY(void) const
+{
+	if(nullptr!=clientPtr)
+	{
+		return state.TxRDY && clientPtr->TxRDY();
+	}
+	return false;
+}
+bool i8251::TxEMPTY(void) const
+{
+	return state.TxEMPTY;
+}
+bool i8251::DSR(void) const
+{
+	if(nullptr!=clientPtr)
+	{
+		return clientPtr->DSR();
+	}
+	return false;
+}
---
+++ src/i8251/i8251.h
@@ -0,0 +1,171 @@
+#ifndef I8251_IS_INCLUDED
+#define I8251_IS_INCLUDED
+/* { */
+
+class i8251
+{
+public:
+	enum
+	{
+		MODESEL_ASYNC_S= 0xC0,
+		MODESEL_ASYNC_S2=0x80,
+		MODESEL_ASYNC_S1=0x40,
+		MODESEL_ASYNC_S_SHIFT=6,
+
+		MODESEL_SYNC_SCS=0x80,
+		MODESEL_SYNC_ESD=0x40,
+
+		MODESEL_EP=      0x20,  // Even Parity
+		MODESEL_PEN=     0x10,  // Parity Enabled
+		MODESEL_L=       0x0C,
+		MODESEL_L2=      0x08,
+		MODESEL_L1=      0x04,
+		MODESEL_L_SHIFT=    2,
+		MODESEL_B=       0x03,
+		MODESEL_B2=      0x02,
+		MODESEL_B1=      0x01,
+		MODESEL_B_SHIFT=    0,
+
+		CMD_WRITE_EH=   0x80,
+		CMD_WRITE_RESET=0x40,
+		CMD_WRITE_RTS=  0x20,
+		CMD_WRITE_CLRER=0x10,
+		CMD_WRITE_SBRK= 0x08,
+		CMD_WRITE_RXEN= 0x04,
+		CMD_WRITE_DTR=  0x02,
+		CMD_WRITE_TXEN= 0x01,
+
+		STATUS_READ_TXRDY= 0x01,
+		STATUS_READ_RXRDY= 0x02,
+		STATUS_READ_TXEMPTY= 0x04,
+		STATUS_READ_PE=    0x08,
+		STATUS_READ_OE=    0x10,
+		STATUS_READ_FE=    0x20,
+		STATUS_READ_SYNDET=0x40,
+		STATUS_READ_DSR=   0x80,
+	};
+
+	enum
+	{
+		STOPBITS_NONE =0,
+		STOPBITS_1    =1,
+		STOPBITS_1HALF=2,
+		STOPBITS_2    =3,
+	};
+
+	/*! Client class should implement a device connected from the serial port.
+	*/
+	class Client
+	{
+	public:
+		/*! i8251 class will call this function to see if the client is ready to receive a byte.
+		*/
+		virtual bool TxRDY(void) const=0;
+
+		/*! i8251 class will call this function when a data is written from the VM.
+		    This function is called when the VM writes a byte to the data register.
+		    It can happen regardless of TxRDY.
+		*/
+		virtual void Tx(unsigned char data) const=0;
+
+		/*! i8251 class will call when the VM sets stop bits.
+		    stopBits is one of STOPBITS_NONE, STOPBITS_1, STOPBITS_1HALF, or STOPBITS_2.
+		*/
+		virtual void SetStopBits(unsigned char stopBits)=0;
+
+		/*!
+		*/
+		virtual void SetParity(bool enabled,bool evenParity)=0;
+
+		/*! bitsPerData is 5,6,7, or 8.
+		*/
+		virtual void SetDataLength(unsigned char bitsPerData)=0;
+
+		/*! This function will be called when VM changes the baud rate.
+		*/
+		virtual void SetBaudRate(unsigned int baudRate)=0;
+
+		/*! i8251 will call this function when a command byte is written from the VM.
+		    This can be used if the data is forwarded to an actual serial port.
+		*/
+		virtual void Command(bool RTS,bool DTR,bool BREAK)=0;
+
+		/*! It should return true if there is a data transmitted from the host to VM.
+		*/
+		virtual bool RxRDY(void) const=0;
+
+		/*! It should return a data transmitted from the host to VM.
+		    This function is called when the VM reads the data bus of i8251 regardless of RxRDY.
+		*/
+		virtual unsigned char Rx(void)=0;
+
+		/*!
+		*/
+		virtual bool DSR(void) const=0;
+	};
+
+	class State
+	{
+	public:
+		long long int lastTxTime; // In nano seconds.
+		long long int lastRxTime; // in nano seconds.
+
+		bool immediatelyAfterReset=true;
+
+		// Must depend on the baud rate, but the emulator can prescribe here.
+		unsigned int nanoSecondsPerByte=8583; // 8583:Roubnly 1M bps.  Emulator can do anything.
+
+		// Baud rate for recording VM setting.
+		unsigned int baudRate=19200;
+
+		// Asynchronous mode states
+		unsigned char stopBits;
+		unsigned char preScale;   // Prescale==0 means synchronous mode.
+
+		// Synchronous mode states
+		bool SCS;
+		bool ESD;
+
+		// Common states
+		unsigned char dataLength; // Number of bits. 5,6,7 or 8.  Only supports 8-bit mode.
+		bool evenParity;
+		bool parityEnabled;
+
+		bool RxEN,TxEN;
+		bool RxRDY,TxRDY;
+		bool SYNDET,FE,OE,PE,TxEMPTY;
+		bool RTS,DTR,BREAK;
+	};
+
+	Client *clientPtr=nullptr;
+
+	State state;
+
+	i8251();
+
+	void Reset(void);
+
+	/*! Recommended to Update(VMTime); before calling this function.
+	*/
+	void VMWriteCommnand(unsigned char data);
+
+	/*! Recommended to Update(VMTime); before calling this function.
+	*/
+	unsigned char VMReadState(void) const;
+
+	void VMWriteData(unsigned char data,long long int VMTime);
+	unsigned char VMReadData(long long int VMTime);
+
+	/*! Updates the device state.
+	    newT is the time in nano seconds.
+	*/
+	void Update(long long int newVMTime);
+
+	bool RxRDY(void) const;
+	bool TxRDY(void) const;
+	bool TxEMPTY(void) const;
+	bool DSR(void) const;
+};
+
+/* } */
+#endif
