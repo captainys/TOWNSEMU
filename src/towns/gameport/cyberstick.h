0a127ade79f51bb4d8fcc651bec5d0eea1e4f658
CaptainYS <PEB01130@nifty.com>
2020-08-17 23:51:16 -0400

    Adding Cyber Stick support.

--- src/towns/gameport/cyberstick.h
+++ src/towns/gameport/cyberstick.h
@@ -10,18 +10,29 @@ public:
 		PHASE_IDLE,
 		PHASE_COMRESET,
 		PHASE_COMSET,
+
+		STROBE_MASK=0x10,
+		STROBE_ACKBIT=0x20,
+
+		DATA_TRANSMISSION_BYTES=11,
 	};
 
 	class State
 	{
-		float x,y,pow;
-		bool buttons[16];
+	public:
+		float x=0,y=0,pow=0;
+		unsigned int buttons=0;
 
-		bool COMOut;  // COM out from Towns
-		unsigned int phase,readCount;
-		unsigned char STROBE;
+		bool COMOutFromCPU=false;
+		unsigned int phase=0,readCount=0;
+		unsigned char STROBE=0;
+		bool ackRead=false;
 	};
 	State state;
+
+	void COMWrite(bool COMOutFromCPU);
+	unsigned char Read(void);
+	unsigned char DataByte(unsigned int readCount) const;
 };
 
 /* } */
