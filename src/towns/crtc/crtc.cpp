5b5c8b152a060babfc3d6397ff24a0297f05ee4b
CaptainYS <PEB01130@nifty.com>
2020-10-18 15:10:27 -0400

    Additional debug info from CRTC

--- src/towns/crtc/crtc.cpp
+++ src/towns/crtc/crtc.cpp
@@ -1166,6 +1166,9 @@ std::vector <std::string> TownsCRTC::GetStatusText(void) const
 	text.back()+=cpputil::Itoa(state.sifter[0]&3)+" "+cpputil::Itoa((state.sifter[0]>>2)&3);
 
 	text.push_back("");
+	text.back()+="FMR VRAM Display Offset:"+cpputil::Ubtox(state.FMRVRAMOffset);
+
+	text.push_back("");
 	text.back()+="FMR VRAM Display Planes:"+cpputil::Ubtox(state.FMRGVRAMDisplayPlanes);
 
 	return text;
