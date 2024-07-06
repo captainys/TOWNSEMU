59571575df75186a85bacd9308d2f571fea915ff
Soji Yamakawa <soji@andrew.cmu.edu>
2024-07-06 19:50:19 -0400

    Corrected typos.

--- src/towns/townstips/townstips.cpp
+++ src/towns/townstips/townstips.cpp
@@ -11,7 +11,7 @@ std::vector <std::string> TownsTips::GetTips(void)
 		"unplug a mouse from the game ports in real FM TOWNS or Tsugaru.\n"
 		"Make sure to unplug a mouse from the game ports when you run\n"
 		"these game titles.\n"
-		"In Tsugaru, go to Game-Port option, and make sure no mouse is\n"
+		"In Tsugaru, go to Game-Port options and make sure no mouse is\n"
 		"selected, or you can select ASUKA120 from application-specific\n"
 		"augmentation.\n"
 	);
@@ -29,11 +29,11 @@ std::vector <std::string> TownsTips::GetTips(void)
 	tips.push_back(
 		"RUN386.EXE in Towns OS V2.1 L10 does not run with 80486 or newer CPU.\n"
 		"Use 'Pretend 80386DX' option to run Towns OS V2.1 L10 on Tsugaru.\n"
-		"Freeward Collection 4 Disc B boots up in this version Towns OS.\n"
+		"Freeware Collection 4 Disc B boots up in this version Towns OS.\n"
 	);
 
 	tips.push_back(
-		"Sherlock Holmes's Consulting Detective crashes if the CD speed is\n"
+		"Sherlock Holmes's Consulting Detective crashes if the CD is\n"
 		"faster than 1X speed.  Please select 1X speed in the option if you\n"
 		"want to run it.  (In real TOWNS, use the compatible mode.)\n"
 		"If the CD is too fast, the read-ahead buffer fills up too fast\n"
@@ -42,10 +42,10 @@ std::vector <std::string> TownsTips::GetTips(void)
 
 	tips.push_back(
 		"You might notice a dark horizontal line about 1/3 from the bottom\n"
-		"of the window of Tsugaru.  It is emulating what's called damper-wire\n"
+		"of the Tsugaru window.  It is emulating what's called a damper-wire\n"
 		"line.  Fujitsu selected the best-quality monitor available for FM TOWNS,\n"
-		"which was SONY's Trinitron CRT.  Only hiccup was the trinitron CRT had \n"
-		"a wire inside the tube called damper wire, which casted a shadow on the\n"
+		"which was SONY's Trinitron CRT.  The only hiccup was the trinitron CRT had \n"
+		"a wire inside the tube called damper wire, which cast a shadow on the\n"
 		"monitor.  However, it was a signature of a high-quality monitor in 1989.\n"
 		"So, Tsugaru emulates it by default.  If you don't like to see the damper-\n"
 		"wire line, please turn off 'Damper-Wire Line' option.\n"
