4adc04a76e2d157cd2eb615d450decd987132db1
Soji Yamakawa <soji@andrew.cmu.edu>
2025-08-06 18:37:37 -0400

    Adding Japanese localization.

---
+++ src/miscutil/uitxt.cpp
@@ -0,0 +1,90 @@
+#include <fstream>
+#include "uitxt.h"
+
+void UiText::clear(void)
+{
+	std::map <std::string,std::string> empty;
+	std::swap(empty,textTable);
+}
+bool UiText::Load(std::string fileName)
+{
+	std::ifstream ifp(fileName);
+	if(true==ifp.is_open())
+	{
+		// clear(); Let's not clear so that multiple uitxts can be used.
+
+		bool firstLine=true;
+		std::string line;
+		while(true!=ifp.eof())
+		{
+			std::getline(ifp,line);
+			if(""==line)
+			{
+				continue;
+			}
+
+			if(true==firstLine)
+			{
+				if("UITXT"!=line)
+				{
+					return false;
+				}
+				firstLine=false;
+			}
+			else
+			{
+				if('#'==line[0])
+				{
+					continue;
+				}
+				if('+'==line[0])
+				{
+					std::string tag,msg;
+					int state=0;
+					for(auto c : line)
+					{
+						switch(state)
+						{
+						case 0:
+							// Skip '+'
+							++state;
+							break;
+						case 1:
+							if(' '==c || '\t'==c)
+							{
+								++state;
+							}
+							else
+							{
+								tag.push_back(c);
+							}
+							break;
+						case 2:
+							if(' '!=c && '\t'!=c)
+							{
+								++state;
+							}
+							break;
+						case 3:
+							msg.push_back(c);
+							break;
+						}
+					}
+					textTable[tag]=msg;
+				}
+			}
+		}
+
+		return true;
+	}
+	return false;
+}
+std::string UiText::operator()(std::string tag,std::string dflt)
+{
+	auto found=textTable.find(tag);
+	if(textTable.end()==found)
+	{
+		return dflt;
+	}
+	return found->second;
+}
---
+++ src/miscutil/uitxt.h
@@ -0,0 +1,25 @@
+#ifndef TSUGARU_UITXT_H_IS_INCLUDED
+#define TSUGARU_UITXT_H_IS_INCLUDED
+
+#include <string>
+#include <map>
+
+/*
+UITXT Format
+
+UITXT  First line.  File ID.
+# Comment line
++tag Message. Tag and message must be separated by space or tab.
+*/
+
+class UiText
+{
+private:
+	std::map <std::string,std::string> textTable;
+public:
+	void clear(void);
+	bool Load(std::string fileName);
+	std::string operator()(std::string tag,std::string dflt);
+};
+
+#endif
