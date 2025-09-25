7104d695591acb0b745169e07525c8a941d268c4
Soji Yamakawa <soji@andrew.cmu.edu>
2025-09-25 16:41:31 -0400

    Mode cache.

--- src/cpu/i486fidelity.h
+++ src/cpu/i486fidelity.h
@@ -849,7 +849,7 @@ public:
 	// High fidelity level considers Protected Mode && IOPL<CPL for Windows 3.1 protected-mode interrupt handlers.
 	static inline bool TakeIOReadException(i486DXFidelityLayer<THISCLASS> &cpu,unsigned int ioport,unsigned int accessSize,Memory &mem,unsigned int numInstBytes)
 	{
-		if(true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86) || cpu.GetIOPL()<cpu.state.CS().DPL))
+		if(i486DXCommon::MODE_VM86==cpu.state.mode || cpu.GetIOPL()<cpu.state.CS().DPL) // <-> true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86)
 		{
 			if(true!=cpu.TestIOMapPermission(cpu.state.TR,ioport,accessSize,mem))
 			{
@@ -864,7 +864,7 @@ public:
 	// High fidelity level considers Protected Mode && IOPL<CPL for Windows 3.1 protected-mode interrupt handlers.
 	static inline bool TakeIOWriteException(i486DXFidelityLayer<THISCLASS> &cpu,unsigned int ioport,unsigned int accessSize,Memory &mem,unsigned int numInstBytes)
 	{
-		if(true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86) || cpu.GetIOPL()<cpu.state.CS().DPL))
+		if(i486DXCommon::MODE_VM86==cpu.state.mode || cpu.GetIOPL()<cpu.state.CS().DPL) // <-> true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86)
 		{
 			if(true!=cpu.TestIOMapPermission(cpu.state.TR,ioport,accessSize,mem))
 			{
@@ -938,7 +938,7 @@ public:
 
 	inline static bool SLDT_STR_LLDT_LTR_VERR_VERW_Cause_INT6_InRealModeVM86Mode(i486DXCommon &cpu)
 	{
-		if(true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86)))
+		if(i486DXCommon::MODE_VM86==cpu.state.mode) // <->(true!=cpu.IsInRealMode() && (0!=(cpu.state.EFLAGS&i486DXCommon::EFLAGS_VIRTUAL86)))
 		{
 			return true;
 		}
