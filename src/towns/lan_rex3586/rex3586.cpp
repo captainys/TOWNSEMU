114186d2f9516410051d01f6c9584467e2430368
Soji Yamakawa <soji@andrew.cmu.edu>
2026-05-07 11:47:50 -0400

    Added rex3586 library.  Still don't know if I can really add support.

--- src/towns/lan_rex3586/rex3586.cpp
+++ src/towns/lan_rex3586/rex3586.cpp
@@ -12,58 +12,50 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 << LICENSE */
-#ifndef REX3586_IS_INCLUDED
-#define REX3586_IS_INCLUDED
-/* { */
 
 
-#include <vector>
-#include <cstdint>
+#include "rex3586.h"
 
-#include "towns.h"
-#include "device.h"
-#include "cheapmath.h"
 
-/* From the real REX3586.  ROM (IO 7010h) returns the following bits.  Low-bit first.
-
-First 20 bits:  0xFFFF0
-Next 40 bits: 0x?????????? (Mac address)
-Next 16 bits: 0xA200
-Next 152 bits:  All 1
-Next 24 bits: 0x4C414E ("LAN")
-Last 4 bits: All 0
-*/
-
-class RatocREX3586 : public Device
+RatocREX3586::RatocREX3586(class FMTownsCommon *ptr) : Device(ptr)
 {
-public:
-	enum
+}
+
+void RatocREX3586::PowerOn(void)
+{
+	for(auto &row : state.regs)
 	{
-		NUM_REG_BANKS=4,
-		NUM_REGS=0x20,
-	};
+		for(auto &col : row)
+		{
+			col=0xFF;
+		}
+	}
+}
 
-	class State
-	{
-	public:
-		unsigned char regs[NUM_REG_BANKS][NUM_REGS];
-	};
-	State state;
+void RatocREX3586::Reset(void)
+{
+}
 
-	virtual const char *DeviceName(void) const{return "REX3586";}
+void RatocREX3586::IOWriteByte(unsigned int ioport,unsigned int data)
+{
+}
 
-	RatocREX3586(class FMTownsCommon *ptr);
+unsigned int RatocREX3586::IOReadByte(unsigned int ioport)
+{
+	return 0xFF;
+}
 
-	void PowerOn(void) override;
-	void Reset(void) override;
 
-	void IOWriteByte(unsigned int ioport,unsigned int data) override;
-	unsigned int IOReadByte(unsigned int ioport) override;
+uint32_t RatocREX3586::SerializeVersion(void) const
+{
+	return 0;
+}
 
-	uint32_t SerializeVersion(void) const override;
-	void SpecificSerialize(std::vector <unsigned char> &data,std::string stateFName) const override;
-	bool SpecificDeserialize(const unsigned char *&data,std::string stateFName,uint32_t version) override;
-};
+void RatocREX3586::SpecificSerialize(std::vector <unsigned char> &data,std::string stateFName) const
+{
+}
 
-/* } */
-#endif
+bool RatocREX3586::SpecificDeserialize(const unsigned char *&data,std::string stateFName,uint32_t version)
+{
+	return true;
+}
