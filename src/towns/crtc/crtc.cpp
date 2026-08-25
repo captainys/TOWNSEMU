02816b0c01c647407bdf87f08f927d8132e5fe97
Soji Yamakawa <soji@andrew.cmu.edu>
2026-08-25 00:30:25 -0400

    Fixed the situation when MX's high-res CRTC is used after FMT-3632 is used.

--- src/towns/crtc/crtc.cpp
+++ src/towns/crtc/crtc.cpp
@@ -626,6 +626,7 @@ void TownsCRTC::MakeLowResPageLayerInfo(Layer &layer,unsigned char page) const
 {
 	page&=1;
 	layer.bitsPerPixel=GetPageBitsPerPixel(page);
+	layer.highColor565=false;
 	layer.highResRGBSwap=0; // Not used by the low-res screen modes.
 	layer.originOnMonitor=GetLowResPageOriginOnMonitor(page);
 	layer.sizeOnMonitor=GetPageSizeOnMonitor(page);
@@ -1987,6 +1988,7 @@ unsigned int TownsCRTC::GetHighResPageVRAMAddressOffset(int page) const
 void TownsCRTC::MakeHighResPageLayerInfo(Layer &layer,unsigned char page) const
 {
 	layer.bitsPerPixel=GetHighResVRAMBitsPerPixel(page);
+	layer.highColor565=false;
 
 	layer.highResRGBSwap=state.highResCrtcReg[HIGHRES_REG_RGB_BGR_BRG_OR_ELSE]; // Hopefully meaningful only in the 24-bit color mode.  Figured 2025/07/02
 	layer.VRAMAddr=0x80000*page;
